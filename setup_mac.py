80c3561cc9f12d0331c9f5d8d738460084ae503f
Liz Cooper-Williams <e.cooperwilliams@uq.edu.au>
2018-04-12 11:01:34 +1000

    Mac stuff

--- setup_mac.py
+++ setup_mac.py
@@ -18,24 +18,57 @@
 #
 
 
-__version__='1.0.0'
-
 from os.path import join, dirname
 from setuptools import setup
+from os import getcwd
+from uploader_app import __version__
 
+application_title = 'QBI OPEX XNAT Uploader'
+exe_name='opex_uploader'
 
-plist = dict(CFBundleDisplayName='QBI OPEX XNAT Uploader',
-             NSHumanReadableCopyright='Copyright (2017) Queensland Brain Institute, University of Queensland',
-             CFBundleTypeIconFile=join('resources', 'upload_logo.ico'),
+plist = dict(CFBundleDisplayName=application_title,
+	         CFBundleName=exe_name,
+             NSHumanReadableCopyright='Copyright (2018) Queensland Brain Institute, University of Queensland',
+             CFBundleTypeIconFile='upload_logo.icns',
              CFBundleVersion=__version__)
 
 APP = ['uploader_app.py']
-DATA_FILES = [join(dirname(__file__),'gui','noname.py'), 'resources']
+DATA_FILES = ['gui', 'resources']
+PARENTDIR= join(getcwd(),'.')
 OPTIONS = {'argv_emulation': True, 'plist':plist,
-           'iconfile': join('resources', 'upload_logo.ico'),
+           'iconfile': join('resources','upload_logo.icns'),
            'includes': ['idna.idnadata','numpy.core._methods', 'numpy.lib.format','lxml._elementpath'],
            'excludes': ['PyQt4', 'PyQt5', 'scipy','notebook','matplotlib','mpl-data','sqlalchemy'],
            'packages': ['pandas','pyxnat','xlrd','urllib','ast','math','lxml','pytz','six','numpy']
            }
 
-setup(app=APP, data_files=DATA_FILES,options={'py2app':OPTIONS}, setup_requires=['py2app'])
+setup(app=APP, data_files=DATA_FILES,options={'py2app':OPTIONS},
+      setup_requires=['py2app', 'pyobjc-framework-Cocoa', 'numpy', 'scipy'],)
+
+# Add info to MacOSX plist
+# plist = Plist.fromFile('Info.plist')
+plist = dict(CFBundleDisplayName=application_title,
+	         CFBundleName=exe_name,
+             NSHumanReadableCopyright='Copyright (c) 2018 Queensland Brain Institute',
+             CFBundleTypeIconFile='measure.ico.icns',
+             CFBundleVersion=__version__
+             )
+
+APP = ['runapp.py']
+DATA_FILES = ['resources', 'gui']
+PARENTDIR= join(getcwd(),'.')
+OPTIONS = {'argv_emulation': True,
+           'plist': plist,
+           'iconfile': 'resources/measure.ico.icns',
+           'packages': ['scipy', 'wx','pandas','msdapp'],
+           'includes':['six','appdirs','packaging','packaging.version','packaging.specifiers','packaging.requirements','os','numbers','future_builtins'],
+           'bdist_base': join(PARENTDIR, 'build'),
+           'dist_dir': join(PARENTDIR, 'dist'),
+           }
+
+setup( name=exe_name,
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app','pyobjc-framework-Cocoa','numpy','scipy'],
+)
