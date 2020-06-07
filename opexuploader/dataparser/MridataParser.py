55d6cf42a63cc1afb42dd178f00ef3b80f4d5bff
Liz Cooper-Williams <Liz.Cooper-Williams@csiro.au>
2020-06-07 17:11:44 +1000

    Added FMRI Data Parser to MridataParser

--- opexuploader/dataparser/MridataParser.py
+++ opexuploader/dataparser/MridataParser.py
@@ -13,55 +13,35 @@ Created on Thu Mar 2 2017
 import argparse
 import glob
 from datetime import datetime
-from os import R_OK, access
-from os.path import join
-import re
+from os import R_OK, access, path
+import logging
 import pandas as pd
-from opexuploader.dataparser.abstract.DataParser import DataParser, stripspaces
 
+from opexuploader.dataparser.abstract.DataParser import DataParser
 
-def extract_subj(string):
-    subject = re.search("(\\d{4})+([A-z]{2})", string).group(0)
-    interval = re.search("(?<=ses-).*", string).group(0)
-    if interval == '01':
-        interval = '0'
-
-    return (subject, int(interval))
-#
-# extract_subj("sub-1001DS_ses-06")
-#
-#
-# subject = re.search("(\\d{4})+([A-z]{2})","sub-1001DS_ses-01").group(0)
-# re.search("(?<=ses-).*", "sub-1001DS_ses-01").group(0)
-#
-#
-# inputdir = r"Q:\DATA\DATA ENTRY\XnatUploaded\sampledata\mridata"
-#
-# df = pd.read_excel(join(inputdir, "ASHS_xs_volumes.xlsx"))
-# df[['Subject','interval']] = df['subjname'].apply(lambda s: extract_subj(s)).apply(pd.Series)
-# sheet = 1
-# skip = 0
-# header = None
-# f2 = join(inputdir, "ASHS_xs_volumes.xlsx")
-# etype = 'MRI ASHS'
-#
-# MridataParser(f2, sheet, skip, header, etype).fields
-#
-# ['Subject', 'interval'] + MridataParser(f2, sheet, skip, header, etype).fields
 VERBOSE = 0
+
 class MridataParser(DataParser):
     def __init__(self, *args):
         DataParser.__init__(self, *args)
-        df = self.data
-        df[['Subject', 'interval']] = df['subjname'].apply(lambda s: extract_subj(s)).apply(pd.Series)
-        # cols = ['Subject', 'interval'] + self.fields
-
-        self.data = df
+        # Replace column name Visit with interval if exists
+        if 'Visit' in self.data.columns:
+            self.data.rename(columns={'Visit': 'interval'}, inplace=True)
+        # remove any spaces in 'interval' and 'version' column names
+        self.data.rename(columns={'version ': 'version', 'interval ': 'interval'}, inplace=True)
         self.sortSubjects('Subject')
-        if self.getPrefix() =='ASHS':
+        if self.getPrefix() == 'ASHS':
             self.data = self.normalizeICV()
-        #sort subjects
-        self.sortSubjects('Subject')
+        elif self.getPrefix() == 'FMRI':
+            # check if column headers need mapping to XNAT fields
+            if self.fields[0] not in self.data.columns:
+                df_fieldmap = pd.read_csv(path.join(self.resource_dir, 'fields', 'MRI_fields.csv'))
+                if df_fieldmap.columns[0] in self.data.columns:
+                    cols = dict(zip(df_fieldmap.FMRImap, df_fieldmap.FMRI))
+                    self.data.rename(columns=cols, inplace=True)
+                else:
+                    msg = 'FMRI fields do not correspond to XNAT fields or FMRImap fields in MRI_fields.csv - exiting'
+                    raise ValueError(msg)
 
 
     def normalizeICV(self):
@@ -78,7 +58,8 @@ class MridataParser(DataParser):
             for lr in ['left', 'right']:
                 lrcols = [lr + c for c in cols]
                 self.data[lr + '_Hippoc'] = self.data.apply(lambda x: (x.loc[lrcols].sum()), axis=1)
-            self.data['Total_Hippoc'] = self.data.apply(lambda x: (x.loc[['left_Hippoc', 'right_Hippoc']].sum()), axis=1)
+            self.data['Total_Hippoc'] = self.data.apply(lambda x: (x.loc[['left_Hippoc', 'right_Hippoc']].sum()),
+                                                        axis=1)
             # Divide all values by ICV
             starti = 8
             endi = len(self.data.columns)
@@ -92,16 +73,20 @@ class MridataParser(DataParser):
         else:
             msg = 'No data found'
         print(msg)
-        #logging.info(msg)
+        logging.info(msg)
         return df
 
     def formatDateString(self, orig):
-        '''Reformats datetime string from yyyy.mm.dd hh:mm:ss to yyyy-mm-dd'''
+        """
+        Reformats datetime string from yyyy.mm.dd hh:mm:ss to yyyy-mm-dd
+        """
         dt = datetime.strptime(orig, "%Y.%m.%d %H:%M:%S")
         return dt.strftime("%Y-%m-%d")
 
     def getStringDateUTC(self, orig):
-        '''Returns datetime string as a unique id'''
+        """
+        Returns datetime string as a unique id
+        """
         dt = datetime.strptime(orig, "%Y.%m.%d %H:%M:%S UTC")
         return dt.strftime("%Y%m%d%H%M%S")
 
@@ -118,7 +103,9 @@ class MridataParser(DataParser):
         return dt.strftime("%Y-%m-%d")
 
     def getInterval(self, orig):
-        """Parses string M-00 to 0 or M-01 to 1 etc"""
+        """
+        Parses string M-00 to 0 or M-01 to 1 etc
+        """
         interval = int(orig[2:])
         return interval
 
@@ -129,12 +116,21 @@ class MridataParser(DataParser):
         :param row: row data with unique number
         :return: id
         """
-        cantabid = sd + '_' + str(row['Visit'])
-        return cantabid
+        sampleid = self.getPrefix() + '_' + sd + '_' + str(row['interval'])
+        if hasattr(row, 'version') and len(row['version']) > 0:
+            sampleid += '_' + str(row['version'])
+        return sampleid
 
     def getxsd(self):
+        """
+        Get dict of opex namespaces
+        :return:
+        """
         xsd = {'ASHS': 'opex:mriashs',
-               'FS': 'opex:mrifs'
+               'FS': 'opex:mrifs',
+               'FMRI': 'opex:fmri',
+               'TASKRET': 'opex:fmritaskret',
+               'TASKENCODE': 'opex:fmritaskencode'
                }
         return xsd
 
@@ -144,13 +140,18 @@ class MridataParser(DataParser):
         :param row: pandas series row data
         :return: data kwargs structure to load to xnat expt
         """
-
+        if hasattr(row, 'version'):
+            version = 'Version %s' % str(row['version'])
+            sample_id = '%s_%s' % (str(i), str(row['version']))
+        else:
+            version = ''
+            sample_id = str(i)
         mandata = {
-            xsd + '/interval': str(row['Visit']),
-            xsd + '/sample_id': str(i),  # row number in this data file for reference
-            xsd + '/sample_quality': 'Unknown',  # default - check later if an error
+            xsd + '/interval': str(row['interval']),
+            xsd + '/sample_id': sample_id,  # row number in this data file for reference
+            xsd + '/sample_quality': 'Good',  # default - check later if an error
             xsd + '/data_valid': 'Initial',
-            xsd + '/comments': ""
+            xsd + '/comments': version
         }
         motdata = {}
         for ctab in self.fields:
@@ -178,46 +179,69 @@ class MridataParser(DataParser):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Parse MRI Analysis',
                                      description='''\
-            Reads files in a directory and extracts data for upload to XNAT
-
+            Reads files in a directory and extracts data for upload to XNAT for the following:
+                  1. MRI analysis data - ASHS
+                  2. MRI analysis data - FreeSurfer
+                  3. MRI analysis data - fMRI behaviour
+                  4. MRI analysis data - fMRI TaskRet
+                  
+            Fields for each type are shown in MRI_fields.csv.  These need to be present in the upload file.
+            Include columns for 'SUBJECT, INTERVAL, VERSION' 
+            Example data: '1001DS', 12, 'MZ2020'
+            (VERSION can be blank - but allows multiple versions for the same subject-interval and will be appended to the ID)
              ''')
-    parser.add_argument('--filedir', action='store', help='Directory containing files', default="..\\sampledata\\mridata")
+    parser.add_argument('--filedir', action='store', help='Directory containing files',
+                        default="..\\sampledata\\mridata")
 
     args = parser.parse_args()
     inputdir = args.filedir
-    print(("Input:", inputdir))
+    print("Input:", inputdir)
     if access(inputdir, R_OK):
         sheet = 1
         skip = 0
         header = None
 
-        seriespattern = '*.xlsx' # originally .csv
+        seriespattern = '*.xlsx'  # originally .csv
         try:
-            files = glob.glob(join(inputdir, seriespattern))
-            print(("Files:", len(files)))
+            files = glob.glob(path.join(inputdir, seriespattern))
+            print("Files:", len(files))
             for f2 in files:
-                print(("Loading ", f2))
+                print("Loading ", f2)
                 etype = 'MRI'
-                if 'ASHS' in f2:
+                if 'ASHS'.lower() in f2.lower():
                     etype += ' ASHS'
-                elif 'FreeSurf' in f2:
+                elif 'FreeSurf'.lower() in f2.lower():
                     etype += ' FS'
+                elif 'FMRI'.lower() in f2.lower():
+                    etype = 'FMRI'
+                    sheet = 0
                 else:
-                    raise ValueError("Cannot determine MRI type")
+                    raise ValueError("Cannot determine MRI type from filename: requires one of ASHS, FreeSurf, FMRI in the xlsx filename")
                 msg = "Running %s" % etype
                 print(msg)
                 dp = MridataParser(f2, sheet, skip, header, etype)
-
+                xsd = dp.getxsd()[etype]
+                print('XSD: %s' % xsd)
                 for sd in dp.subjects:
                     for i, row in dp.subjects[sd].iterrows():
-                        msg = '**SubjectID: %s Visit: %d' % ( sd, int(row['Visit']))
+                        msg = '**SubjectID: %s Visit: %dm' % (sd, int(row['interval']))
                         print(msg)
+                        sampleid = dp.getSampleid(sd, row)
+                        print(sampleid)
+                        # Print every field value for every subject
                         for ctab in dp.fields:
                             if ctab in row:
                                 msg1 = "Field: %s = %d" % (ctab, row[ctab])
                                 print(msg1)
+                        # Print XNAT upload data for every subject
+                        (mandata, motdata) = dp.mapData(row, i, xsd)
+                        print(mandata)
+                        print(motdata)
+                    # just print one subject
+                    break
+
 
         except Exception as e:
-            print(("ERROR: ", e))
+            print("ERROR: ", e)
     else:
-        print(("Cannot access directory: ", inputdir))
+        print("Cannot access directory: ", inputdir)
